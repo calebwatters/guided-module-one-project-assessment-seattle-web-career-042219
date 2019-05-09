7bd44c76ccfff3a38da8f425df78242a85b5089b
github username <github email address>
2019-05-09 14:55:41 -0700

    made delete adoption option

--- lib/command_line.rb
+++ lib/command_line.rb
@@ -234,26 +234,39 @@ end
 
 #'question' prompts the user for the means by which they would like to search for a character
 
-def question
-  puts <<-EOF
-    Press 1. to search by name
-    Press 2. to search by breed
-    Press 3. to search a random character
-  EOF
-  answer = gets.chomp
+def adoption_search_prompt
+  prompt = TTY::Prompt.new
+  options_array = ["Search by name", "Search by breed", "Wildcard"]
+  options_list = options_array.each {|string| string.to_s}
+  search_query = prompt.select("Select your search method", options_list)
+  search_query
+end
 
-  if answer == "1"
+def route_search(search_query)
+  if search_query == "Search by name"
     find_characters_by_name
 
-  elsif answer == "2"
+  elsif
+    search_query == "Search by breed"
     get_breed_from_user
-  elsif answer == "3"
-    search_by_breed("11")
-  else
-    puts "that is not a valid input yet"
+  elsif
+    search_query== "Wildcard"
+    puts <<-EOF
+   ---------------------------------------------------------------------
+   You wildling, you!! Livin on the edge huh?? What if you get Geoffry?!
+   ---------------------------------------------------------------------
+   EOF
+  CLI.current_result = Character.all.sample
+  search_results(CLI.current_result)
+  adopt?
   end
+end
+
+def question
+  route_search(adoption_search_prompt)
 
 end
+
 
 #'GoT_logo' is pretty self explanitory
 
@@ -429,7 +442,7 @@ def delete_pet
     pet = Character.find_by_name(answer)
     pet_name = pet.name
 
-    Character.delete(pet.id)
+    Adoption.where(character_id: pet.id, user_id: CLI.current_user.id).destroy_all
     puts <<-EOF
 
            #{pet_name} has been ABANDONED.
